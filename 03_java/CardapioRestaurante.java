11c3109e92715fad2bde58ef69e3ea89d59c5fe8
kauwfx <20251ctb0100002@estudantes.ifpr.edu.br>
2026-06-09 15:17:14 +0000

    aula 26: segundo programa

--- 03_java/CardapioRestaurante.java
+++ 03_java/CardapioRestaurante.java
@@ -1,69 +1,131 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class CardapioRestaurante {
 
     public static void main(String[] args) {
 
-        // Cria o Scanner para ler dados do teclado
         Scanner entrada = new Scanner(System.in);
+        Random random = new Random();
 
-        // Exibe o cardápio
-        System.out.println("=================================");
-        System.out.println("  AMAZONAS - CARDÁPIO ELETRÔNICO");
-        System.out.println("=================================");
-        System.out.println("1 - X-Burguer .......... R$ 18,00");
-        System.out.println("2 - Pizza .............. R$ 35,00");
-        System.out.println("3 - Suco Natural ....... R$ 8,00");
-        System.out.println("4 - Café ............... R$ 5,00");
-        System.out.println("5 - Refrigerante ....... R$ 4,00");
-        System.out.println("=================================");
+        double valorTotalGeral = 0;
+        String resumoPedido = "";
 
-        // Pede a opção do usuário
-        System.out.print("Escolha uma opção: ");
-        int opcao = entrada.nextInt();
+        boolean sistemaAtivo = true;
 
-        // Variáveis para guardar o nome e o preço do produto
-        String produto = "";
-        double preco = 0;
+        while (sistemaAtivo) {
 
-        // Verifica qual item foi escolhido
-        if (opcao == 1) {
-            produto = "X-Burguer";
-            preco = 18.0;
-        } else if (opcao == 2) {
-            produto = "Pizza";
-            preco = 35.0;
-        } else if (opcao == 3) {
-            produto = "Suco Natural";
-            preco = 8.0;
-        } else if (opcao == 4) {
-            produto = "Café";
-            preco = 5.0;
-        } else if (opcao == 5) {
-            produto = "Refrigerante";
-            preco = 4.0;
-        } else {
-            // Caso a opção não exista
-            System.out.println("Opção inválida!");
-            entrada.close();
-            return;
+            boolean continuarComprando = true;
+
+            while (continuarComprando) {
+
+                System.out.println("===========================");
+                System.out.println("   LANCHONETE JAVA BURGER  ");
+                System.out.println("===========================");
+
+                System.out.println("1 - Hambúrguer Clássico");
+                System.out.println("2 - Cheeseburger Duplo");
+                System.out.println("3 - Batata Frita");
+                System.out.println("4 - Nuggets");
+                System.out.println("5 - Milkshake");
+                System.out.println("6 - Hot Dog Especial");
+                System.out.println("7 - Finalizar Pedido");
+                System.out.println();
+
+                System.out.print("Escolha: ");
+                int opcao = entrada.nextInt();
+
+                if (opcao == 7) {
+                    break;
+                }
+
+                double preco = 0;
+                String itemEscolhido = "";
+
+                if (opcao == 1) {
+                    itemEscolhido = "Hambúrguer Clássico";
+                    preco = 20.00;
+                } else if (opcao == 2) {
+                    itemEscolhido = "Cheeseburger Duplo";
+                    preco = 28.00;
+                } else if (opcao == 3) {
+                    itemEscolhido = "Batata Frita";
+                    preco = 12.00;
+                } else if (opcao == 4) {
+                    itemEscolhido = "Nuggets";
+                    preco = 15.00;
+                } else if (opcao == 5) {
+                    itemEscolhido = "Milkshake";
+                    preco = 14.00;
+                } else if (opcao == 6) {
+                    itemEscolhido = "Hot Dog Especial";
+                    preco = 18.00;
+                } else {
+                    System.out.println("\nOpção inválida! Tente novamente.\n");
+                    continue;
+                }
+
+                System.out.print("\nQuantidade: ");
+                int quantidade = entrada.nextInt();
+
+                double valorItem = preco * quantidade;
+
+                valorTotalGeral += valorItem;
+                resumoPedido += quantidade + "x " + itemEscolhido +
+                        " ..... R$ " + String.format("%.2f", valorItem) + "\n";
+
+                System.out.println("\nItem adicionado ao pedido!\n");
+
+                System.out.println("Deseja continuar comprando?");
+                System.out.println("1 - Sim");
+                System.out.println("2 - Finalizar");
+                System.out.print("Escolha: ");
+                int escolhaContinuar = entrada.nextInt();
+
+                System.out.println("\n===========================");
+
+                if (escolhaContinuar == 2) {
+                    continuarComprando = false;
+                }
+            }
+
+            System.out.println("===========================");
+            System.out.println("   RESUMO DO PEDIDO");
+            System.out.println("===========================");
+
+            if (resumoPedido.equals("")) {
+                System.out.println("Nenhum item foi adicionado.");
+            } else {
+                System.out.print(resumoPedido);
+            }
+
+            System.out.println("\nTOTAL: R$ " + String.format("%.2f", valorTotalGeral));
+            System.out.println();
+
+            System.out.println("Forma de pagamento:\n");
+            System.out.println("1 - Dinheiro");
+            System.out.println("2 - Cartão");
+            System.out.println("3 - PIX");
+            System.out.println("4 - Voltar e alterar o pedido");
+            System.out.print("Escolha: ");
+
+            int formaPagamento = entrada.nextInt();
+
+            if (formaPagamento == 4) {
+                System.out.println("\nVoltando ao menu... pedido mantido!\n");
+                continue;
+            }
+
+            System.out.println("\nPagamento realizado com sucesso!");
+
+            int numeroPedido = random.nextInt(900) + 100;
+            System.out.println("\nPedido Nº " + numeroPedido);
+
+            System.out.println("\nAguarde a chamada do seu pedido.");
+
+            sistemaAtivo = false;
         }
 
-        // Pergunta a quantidade desejada
-        System.out.print("Digite a quantidade: ");
-        int quantidade = entrada.nextInt();
-
-        // Calcula o valor total
-        double total = preco * quantidade;
-
-        // Mostra o resumo do pedido
-        System.out.println("\n===== RESUMO DO PEDIDO =====");
-        System.out.println("Produto: " + produto);
-        System.out.println("Preço: R$ " + preco);
-        System.out.println("Quantidade: " + quantidade);
-        System.out.println("Total: R$ " + total);
-
-        // Fecha o Scanner
         entrada.close();
     }
 }
