7f894be07eb9acd27c6fdc07af967519e7f553d3
kauwfx <20251ctb0100002@estudantes.ifpr.edu.br>
2026-08-27 14:59:11 +0000

    Parte 6: toString e relatorio

--- aula32-projeto-secretaria/Main.java
+++ aula32-projeto-secretaria/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ import java.util.Scanner;
  * Projeto   : aula32-projeto-secretaria
  * Arquivo   : Main.java
  */
+
 public class Main {
 
     public static void main(String[] args) {
